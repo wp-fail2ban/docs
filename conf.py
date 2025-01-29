51be178f75e26365bf4845c5a46289ef287b786c
Charles Lecklider <wordpress@charles.lecklider.org>
2025-01-29 13:30:02 +0000

    Update copyright year range to 2025

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ from pygments.lexers.web import PhpLexer
 # -- Project information -----------------------------------------------------
 
 project = u'WP fail2ban'
-copyright = u'2018-24, Charles Lecklider'
+copyright = u'2018-2025, Charles Lecklider'
 author = u'Charles Lecklider'
 
 # The short X.Y version
